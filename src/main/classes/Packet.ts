7b0753b4450b66b286905179ec19367c39928010
Iamproplayer7 <iamproplayer7@gmail.com>
2024-06-08 01:36:04 +0300

    add class Packet

---
+++ src/main/classes/Packet.ts
@@ -0,0 +1,28 @@
+import { PacketType, Sendable } from "tsinsim/packets";
+import { Server } from "./Server.js";
+
+// STATIC CLASS
+export class Packet {
+    constructor(public x: number, public y: number) { 
+        throw new Error('VISM.Packet is a static class. new VISM.Packet cannot be constructed.');
+    }
+
+    // STATIC START
+    static all: { name: PacketType, callback: (data: any, server: Server) => void }[] = [];
+    static on(name: PacketType | PacketType[], callback: (data: any, server: Server) => void) {
+        if(typeof name == 'number') {
+            this.all.push({ name, callback });
+        }
+        else {
+            for(const n of name) {
+                this.all.push({ name: n, callback });
+            }
+        }
+    }
+
+    static send(Server: Server, packet: Sendable) {
+        if(!Server.InSimHandle) return;
+        Server.InSimHandle.sendPacket(packet);
+    }
+    // STATIC END
+}
