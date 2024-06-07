6f579d12e0f5e960f11a8b9e3975eeefde58818b
Iamproplayer7 <iamproplayer7@gmail.com>
2024-06-08 01:37:16 +0300

    export main

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import { Event, Packet, Player, Server } from './main/index.js';
+
+export * from 'tsinsim';
+export * from './main/enums/index.js';
+
+export const VISM = {
+    Player,
+    Server,
+    Event,
+    Packet,
+}
