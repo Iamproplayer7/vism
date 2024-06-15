b3fc1e571f1c5edd823d54e713b38a20df2fdc3e
Iamproplayer7 <iamproplayer7@gmail.com>
2024-06-15 20:45:15 +0300

    update test

--- test.ts
+++ test.ts
@@ -1,5 +1,5 @@
-import { VISM, PacketType, IS_NCN, IS_MTC, InSimFlags, IS_NCI, EventType, LeaveReason } from './src/index.ts';
-import { Event, Packet, Server } from './src/main/index.ts';
+import { VISM, InSimFlags, EventType, LeaveReason, IS_NPL } from './src/index.ts';
+import { Event, Mod, Vehicle } from './src/main/index.ts';
 
 const server = new VISM.Server({
     Admin: 'test',                      
@@ -10,17 +10,40 @@ const server = new VISM.Server({
     Prefix: 33                        
 });
 
-server.connect('127.0.0.1', 29999);
+server.connect('', 0);
 
+Event.on(EventType.SERVER_CONNECTED, (server) => {
+    Mod.add(server, '5587CD');
+    console.log(Mod.getAll(server));
+    Mod.remove(server, '5587CD');
+    console.log(Mod.getAll(server));
+});
 
 Event.on(EventType.PLAYER_CONNECTING, (player) => {
-    console.log(`${player.Name} is connecting...`);
+    console.log(`${player.getName()} is connecting...`);
 });
 
 Event.on(EventType.PLAYER_CONNECTED, (player) => {
-    console.log(`${player.Name} + ' connected. [UserID: ${player.UserID}]`);
+    console.log(`${player.getName()} connected. [UserID: ${player.getUserID()}]`);
 })
 
 Event.on(EventType.PLAYER_DISCONNECTED, (player, reason) => {
-    console.log(`${player.Name} + ' disconnected. [Reason: ${LeaveReason[reason]}]`);
+    console.log(`${player.getName()} disconnected. [Reason: ${LeaveReason[reason]}]`);
+})
+
+Event.on(EventType.VEHICLE_JOIN_REQUEST, (player, data: IS_NPL) => {
+    console.log(`${player.getName()} requests a vehicle (PLID: ${data.PLID}, Name: ${data.CName}).`);
+    Vehicle.respondToJoinRequest(player, true);
+})
+
+Event.on(EventType.VEHICLE_CREATED, (vehicle, player) => {
+    console.log(`${player.getName()} created a new vehicle (PLID: ${vehicle.getPLID()}, Name: ${vehicle.getName()}).`);
+})
+
+Event.on(EventType.VEHICLE_DESTROYED, (vehicle, player) => {
+    console.log(`${player.getName()} destroyed a vehicle (PLID: ${vehicle.getPLID()}, Name: ${vehicle.getName()}).`);
+})
+
+Event.on(EventType.VEHICLE_UPDATE, (vehicle) => {
+    console.log('speed', vehicle.getSpeed());
 })
