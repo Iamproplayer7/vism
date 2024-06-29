e4f85342b80d6a14596de50934b90d9da2b602cb
Iamproplayer7 <iamproplayer7@gmail.com>
2024-06-29 13:18:55 +0300

    Command.off method

--- src/main/classes/Command.ts
+++ src/main/classes/Command.ts
@@ -14,6 +14,16 @@ export class Command {
         this.all.push({ name, callback });
     }
 
+    static off(name: string) {
+        const command = this.all.find((command) => command.name === name);
+        if(command) {
+            const indexOf = this.all.indexOf(command);
+            if(indexOf !== -1) {
+                this.all.splice(indexOf, 1);
+            }
+        }
+    }
+
     static fire(name: string, ...args: any[]){
         const commands = this.all.filter((command) => command.name === name);
         for(const command of commands) {
@@ -31,8 +41,6 @@ Packet.on(PacketType.ISP_MSO, (data: IS_MSO) => {
     const Text = data.Text.slice(data.TextStart, data.Text.length);
     if(Text.length <= 1) return;
 
-    const cmd = Text.slice(1, Text.length).split(' ')[0];
-    const args = Text.split(' ').slice(1, Text.split(' ').length);
-    
-    Command.fire(cmd, player, ...args);
+    const _ = Text.split(' ');
+    Command.fire(_[0].slice(1, _[0].length), player, ..._.slice(1, _.length));
 });
