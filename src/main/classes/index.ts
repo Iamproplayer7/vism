c43b162626362eae646b6c09025b62daa108d4ea
Iamproplayer7 <iamproplayer7@gmail.com>
2024-08-24 06:40:47 +0300

    export new helpers

--- src/main/classes/index.ts
+++ src/main/classes/index.ts
@@ -1,8 +1,10 @@
 export { Server } from './Server.js';
-export { Player } from './Player.js';
+export * from './Player.js';
+export * from './Vehicle.js';
 export { Packet } from './Packet.js';
 export { Event } from './Event.js';
-export { Vehicle } from './Vehicle.js';
 export { Mod } from './Mod.js';
 export { Layout } from './Layout.js';
 export { Command } from './Command.js';
+export { Button } from './Button.js';
+export { Menu } from './Menu.js';
