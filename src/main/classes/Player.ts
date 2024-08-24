ed89b346f23f806d1a76a4f5b44eef1414b0a9de
Iamproplayer7 <iamproplayer7@gmail.com>
2024-08-24 06:39:12 +0300

    VISM.Player rework

--- src/main/classes/Player.ts
+++ src/main/classes/Player.ts
@@ -1,124 +1,177 @@
-import { PacketType } from "tsinsim";
-import { IS_CIM, IS_CNL, IS_CPR, IS_NCI, IS_NCN } from "tsinsim/packets";
+import { CarFlags, PacketType } from "tsinsim";
+import { IS_CIM, IS_CNL, IS_CPR, IS_MST, IS_MTC, IS_NCI, IS_NCN, IS_PLC } from "tsinsim/packets";
 import { Packet } from "./Packet.js";
 import { Event } from "./Event.js";
 import { EventType } from "../enums/event.js";
 import { Server } from "./Server.js";
+import { Interval } from "../utilities/Interval.js";
+import { Vehicle } from "./Vehicle.js";
 
+export const PlayerGetter = {
+    all: [] as Player[],
 
-export class Player {
-    // STATIC START
-    static all: Player[] = [];
-    static getByUCID(UCID: number) {
-        return Player.all.find((p) => p.UCID === UCID) ?? false;
+    getByUCID(server: Server, UCID: number) {
+        return this.all.find((player) => player.getServer() === server && player.getUCID() === UCID);
+    },
+
+    getByName(server: Server, Name: string) {
+        return this.all.find((player) => player.getServer() === server && player.getName().toLowerCase() === Name.toLowerCase());
+    },
+
+    getByUsername(server: Server, Username: string) {
+        return this.all.find((player) => player.getServer() === server && player.getUsername().toLowerCase() === Username.toLowerCase());
     }
+}
 
-    static getByName(Name: string) {
-        return Player.all.find((p) => p.Name.toLowerCase() === Name.toLowerCase()) ?? false;
-    }
+type Interface = { Mode: number, SubMode: number, SelType: number };
 
-    static getByUsername(Username: string) {
-        return Player.all.find((p) => p.Username.toLowerCase() === Username.toLowerCase()) ?? false;
-    }
-    // STATIC END
+export interface Player {
+    valid: boolean; 
+    getServer(): Server; 
+    getUCID(): number; 
+    getUserID(): number;
+    getName(): string;
+    getUsername(): string;
+    isAdmin(): boolean;
+    getLanguage(): number;
+    getIpAdress(): string;
+    getVehicle(): Vehicle | false;
+    setVehicle(vehicle: Vehicle): void;
+    getInterface(): Interface;
 
-    // DEFAULT VALUES
-    valid: boolean = false;
-    readonly Server: Server;
-    readonly UCID: number;
-    readonly Admin: boolean;
-    readonly Username: string;
-    Name: string;
-    UserID?: number;
-    Language?: number;
-    IpAdress?: string;
-    Interface: { Mode: number, SubMode: number, SelType: number };
-    
+    kick(text: string): void;
+    message(text: string, sound?: number): void;
+
+    // interval
+    setInterval(name: string, callback: () => void, ms: number, fireWhenCreated?: boolean): void;
+    clearInterval(name: string): void;
+    getIntervals(): Interval[];
+
+    allowVehicles(vehiclesFlags: CarFlags[]): void,
+}
+
+class PlayerInternal implements Player  {
+    valid: boolean;
+
+    private Server: Server;
+    private UCID: number;
+    private Username: string;
+    private Admin: boolean;
+    private Name: string;
+
+    private UserID: number = 0;
+    private Language: number = 0;
+    private IpAdress: string = '0.0.0.0';
+
+    private Vehicle: Vehicle | false = false;
+    private Interface: Interface = { Mode: 0, SubMode: 0, SelType: 0 };
+
     constructor(data: IS_NCN, server: Server) {
-        // base
         this.valid = true;
         this.Server = server;
         this.UCID = data.UCID;
         this.Username = data.UName;
         this.Admin = data.Admin == 1;
         this.Name = data.PName;
+        
+        // additional player info
+        Packet.on(PacketType.ISP_NCI, (data: IS_NCI, server: Server) => {
+            if(server !== this.Server || data.UCID !== this.UCID) return;
+        
+            this.UserID = data.UserID;
+            this.Language = data.Language;
+            this.IpAdress = data.IpAddress;
 
-        // interface
-        this.Interface = { Mode: 0, SubMode: 0, SelType: 0 };
+            Event.fire(EventType.PLAYER_CONNECTED, this);
+        }).bind(this);
+
+        // player name change
+        Packet.on(PacketType.ISP_CPR, (data: IS_CPR, server: Server) => {
+            if(server !== this.Server || data.UCID !== this.UCID) return;
+        
+            const Name = this.Name;
+            this.Name = data.PName;
+            Event.fire(EventType.PLAYER_NAME_UPDATE, this, { old: Name, new: data.PName });
+        }).bind(this);
+        
+        // player interface change
+        Packet.on(PacketType.ISP_CIM, (data: IS_CIM, server: Server) => {
+            if(server !== this.Server || data.UCID !== this.UCID) return;
+        
+            this.Interface = { Mode: data.Mode, SubMode: data.SubMode, SelType: data.SelType };
+            Event.fire(EventType.PLAYER_INTERFACE_UPDATE, this, this.Interface);
+        }).bind(this);
+        
+        // player disconnects
+        Packet.on(PacketType.ISP_CNL, (data: IS_CNL, server: Server) => {
+            if(server !== this.Server || data.UCID !== this.UCID) return;
+        
+            Event.fire(EventType.PLAYER_DISCONNECTED, this, data.Reason);
+            this.valid = false;
+
+            Packet.offByBind(this);
+            PlayerGetter.all = PlayerGetter.all.filter((player) => player !== this);
+        }).bind(this);
     }
 
-    // getters
-    public getUCID() {
-        return this.UCID;
+    getServer()                  { return this.Server;    };
+    getUCID()                    { return this.UCID;      };
+    getUserID()                  { return this.UserID;    };
+    getName()                    { return this.Name;      };
+    getUsername()                { return this.Username;  };
+    isAdmin()                    { return this.Admin;     };
+    getLanguage()                { return this.Language;  };
+    getIpAdress()                { return this.IpAdress;  };
+    getVehicle()                 { return this.Vehicle;   };
+    setVehicle(vehicle: Vehicle) { this.Vehicle = vehicle; }
+    getInterface()               { return this.Interface; };
+       
+    setInterval(name: string, callback: () => void, ms: number) {
+        Interval.set(`player-${this.getUCID()}-${name}`, () => {
+            callback();
+        }, ms).bind(this);
+    };
+
+    clearInterval(name: string) {
+        Interval.clear(`player-${this.getUCID()}-${name}`);
     }
 
-    public getUserID() {
-        return this.UserID;
+    getIntervals() {
+        return Interval.all.filter((interval) => interval.name.includes(`player-${this.getUCID()}`));
     }
 
-    public getName() {
-        return this.Name;
+    message(text: string, sound?: number) {
+        this.Server.InSimHandle.sendPacket(new IS_MTC({ UCID: this.UCID, Text: text, Sound: sound ?? 0 }));
     }
 
-    public getUsername() {
-        return this.Username;
+    kick(text: string = '') {
+        if(text = '') {
+            return this.Server.InSimHandle.sendPacket(new IS_MST({ Msg: '/kick ' + this.Username }));
+        }
+
+        this.message(text);
+
+        setTimeout(() => {
+            if(!this.valid) return;
+            this.Server.InSimHandle.sendPacket(new IS_MST({ Msg: '/kick ' + this.Username }));
+        }, 100)
     }
 
-    public isAdmin() {
-        return this.Admin;
-    }
+    allowVehicles(vehiclesFlags: CarFlags[]) {
+        var flags = 0;
+        for(const vehFlag of vehiclesFlags) {
+            flags += vehFlag;
+        }
 
-    public getIpAdress() {
-        return this.IpAdress;
-    }
-
-    public getInterface() {
-        return this.Interface;
+        this.Server.InSimHandle?.sendPacket(new IS_PLC({ UCID: 255, Cars: flags }))
     }
 }
 
+// creating the player
 Packet.on(PacketType.ISP_NCN, (data: IS_NCN, server: Server) => {
     if(data.UCID === 0) return;
     
-    Player.all.push(new Player(data, server));
-    Event.fire(EventType.PLAYER_CONNECTING, Player.getByUCID(data.UCID));
-});
-
-Packet.on(PacketType.ISP_NCI, (data: IS_NCI) => {
-    const player = Player.getByUCID(data.UCID);
-    if(!player) return;
-
-    player.UserID = data.UserID;
-    player.Language = data.Language;
-    player.IpAdress = data.IpAddress;
-
-    Event.fire(EventType.PLAYER_CONNECTED, player);
-});
-
-Packet.on(PacketType.ISP_CPR, (data: IS_CPR) => {
-    const player = Player.getByUCID(data.UCID);
-    if(!player) return;
-
-    const Name = player.Name;
-    player.Name = data.PName;
-    Event.fire(EventType.PLAYER_NAME_UPDATE, player, { old: Name, new: data.PName });
-});
-
-Packet.on(PacketType.ISP_CIM, (data: IS_CIM) => {
-    const player = Player.getByUCID(data.UCID);
-    if(!player) return;
-
-    player.Interface = { Mode: data.Mode, SubMode: data.SubMode, SelType: data.SelType };
-    Event.fire(EventType.PLAYER_INTERFACE_UPDATE, player, player.Interface);
-});
-
-Packet.on(PacketType.ISP_CNL, (data: IS_CNL) => {
-    const player = Player.getByUCID(data.UCID);
-    if(!player) return;
-
-    const indexOf = Player.all.indexOf(player);
-    if(indexOf !== -1) {
-        Event.fire(EventType.PLAYER_DISCONNECTED, Player.getByUCID(data.UCID), data.Reason);
-        Player.all.splice(indexOf, 1);
-    }
+    const player = new PlayerInternal(data, server);
+    PlayerGetter.all.push(player);
+    Event.fire(EventType.PLAYER_CONNECTING, player);
 });
