ca98d1d4beeb91b3d676e440dbd273b437fa3d7a
Iamproplayer7 <iamproplayer7@gmail.com>
2024-08-24 06:39:01 +0300

    VISM.Packet class to -> object

--- src/main/classes/Packet.ts
+++ src/main/classes/Packet.ts
@@ -1,28 +1,47 @@
 import { PacketType, Sendable } from "tsinsim/packets";
 import { Server } from "./Server.js";
 
-// STATIC CLASS
-export class Packet {
-    constructor() { 
-        throw new Error('VISM.Packet is a static class. new VISM.Packet cannot be constructed.');
-    }
+type Packet = { name: PacketType, callback: CallbackFn, bind?: any };
+type CallbackFn = (data: any, server: Server) => void;
 
-    // STATIC START
-    static all: { name: PacketType, callback: (data: any, server: Server) => void }[] = [];
-    static on(name: PacketType | PacketType[], callback: (data: any, server: Server) => void) {
-        if(typeof name == 'number') {
-            this.all.push({ name, callback });
-        }
-        else {
+export const Packet = {
+    all: [] as Packet[],
+
+    on(name: PacketType | PacketType[], callback: CallbackFn) {
+        var created: number[] = [];
+
+        if(Array.isArray(name)) {
             for(const n of name) {
-                this.all.push({ name: n, callback });
+                created.push(this.all.push({ name: n, callback, bind: null })-1);
             }
         }
-    }
+        else {
+            created.push(this.all.push({ name: name, callback, bind: null })-1);
+        }
+        
+        const self = this;
+        return {
+            bind(entity: any) {
+                self.all.forEach((v, i) => {
+                    if(created.includes(i)) {
+                        v.bind = entity;
+                    }
+                })
+            }
+        }
+    },
 
-    static send(Server: Server, packet: Sendable) {
-        if(!Server.InSimHandle) return;
-        Server.InSimHandle.sendPacket(packet);
+    off(name: PacketType) {
+        this.all = this.all.filter((packet) => packet.name !== name);
+    },
+
+    offByBind(entity: any) {
+        this.all.filter((packet) => packet.bind === entity).forEach((packet) => {
+            this.off(packet.name);
+        });
+    },
+
+    send(server: Server, packet: Sendable) {
+        server.InSimHandle.sendPacket(packet);
     }
-    // STATIC END
 }
