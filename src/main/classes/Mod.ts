fb93939ba211d8e99526049e2e7b84a3898f2fa2
Iamproplayer7 <iamproplayer7@gmail.com>
2024-08-24 06:38:49 +0300

    VISM.Mod class -> object

--- src/main/classes/Mod.ts
+++ src/main/classes/Mod.ts
@@ -2,56 +2,30 @@ import { IS_MAL } from "tsinsim/packets";
 import { Packet } from "./Packet.js";
 import { Server } from "./Server.js";
 
-export class Mod {
-    // STATIC START
-    static allowed: { server: Server, id: string }[] = [];
+export const Mod = {
+    allowed: [] as { server: Server, id: string }[],
 
-    static update(server: Server, all: boolean = false) {
-        if(!server.InSimHandle) return;
+    exists(server: Server, id: string) {
+        return this.allowed.find((m) => m.server === server && m.id === id);
+    },
 
-        if(all) {
-            Packet.send(server, new IS_MAL({ SkinID: [] }))
-        }
-        else {
-            Packet.send(server, new IS_MAL({ SkinID: this.allowed.map(m => m.id) }))
-        }
-    }
-
-    static add(server: Server, id: string) {
-        if(this.exists(server, id)) {
-            this.update(server);
-            return;
-        }
-
-        this.allowed.push({ server, id });
-        this.update(server);
-    }
-
-    static remove(server: Server, id: string) {
-        const mod = this.exists(server, id)
-        if(!mod) return;
-
-        const indexOf = this.allowed.indexOf(mod);
-        if(indexOf !== -1) {
-            this.allowed.splice(indexOf, 1);
-            this.update(server);
-        }
-    }
-
-    static allowAll(server: Server) {
+    allowAllMods(server: Server) {
         this.update(server, true);
-    }
+    },
 
-    static exists(server: Server, id: string) {
-        return this.allowed.find((m) => m.server === server && m.id === id) ?? false;
-    }
+    update(server: Server, all = false) {
+        Packet.send(server, new IS_MAL({ SkinID: all ? [] : this.allowed.map(m => m.id) }))
+    },
 
-    static getAll(server: Server) {
-        return this.allowed.filter((m) => m.server === server).map((m) => m.id);
-    }
-    // STATIC END
+    add(server: Server, id: string) {
+        if(!this.exists(server, id)) {
+            this.allowed.push({ server, id });
+        }
+        
+        this.update(server);
+    },
 
-    constructor() { 
-        throw new Error('VISM.Mod is a static class. new VISM.Mod cannot be constructed.');
+    remove(server: Server, id: string) {
+        this.allowed = this.allowed.filter((mod) => mod.server !== server || mod.id !== id);
     }
 }
