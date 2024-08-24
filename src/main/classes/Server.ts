7c60e6e60b61433e46d0cc70ca517b48376fe83a
Iamproplayer7 <iamproplayer7@gmail.com>
2024-08-24 06:39:24 +0300

    VISM.Server class -> object

--- src/main/classes/Server.ts
+++ src/main/classes/Server.ts
@@ -1,43 +1,65 @@
 import { InSim } from "tsinsim";
-import { IS_ISI } from "tsinsim/packets";
+import { IS_ISI, IS_MST, IS_MTC } from "tsinsim/packets";
 import { Packet } from "./Packet.js";
 import { Event } from "./Event.js";
 import { EventType } from "../enums/event.js";
 
-export class Server {
-    // STATIC START
-    static all: Server[] = [];
-    // STATIC END
+export type Server = {
+    InSimHandle: InSim,
 
-    InSimHandle: InSim | null;
+    connect: (Host: string, Port: number) => void,
+    disconnect: () => void,
+    message: (text: string, sound?: number) => void,
+    command: (text: string) => void
+}
 
-    constructor(InSimOptions: Partial<IS_ISI>) {
-        this.InSimHandle = new InSim(InSimOptions);
+export const Server = {
+    all: [] as Server[],
+
+    create(InSimOptions: Partial<IS_ISI>) {
+        const InSimHandle = new InSim(InSimOptions);
+        
+        const ServerObject: Server = {
+            InSimHandle: InSimHandle,
+
+            connect(Host, Port) {
+                console.log('[InSim] Connecting to ' + Host + ':' + Port);
+                this.InSimHandle.connect({ Host, Port });
+            },
+
+            disconnect() {
+                console.log('[InSim] Disconnecting...');
+                this.InSimHandle.disconnect();
+            },
+        
+            message(text, sound) {
+                this.InSimHandle.sendPacket(new IS_MTC({ UCID: 255, Text: text, Sound: sound ?? 0 }));
+            },
+        
+            command(text: string) {
+                this.InSimHandle.sendPacket(new IS_MST({ Msg: text }));
+            }
+        }
 
         // handle InSim events
-        this.InSimHandle.onGlobal((name, data) => {
-            for(const packet of Packet.all) {
-                if(packet.name == name) {
-                    packet.callback(data, this);
-                }
+        InSimHandle.onGlobal((name, data) => {
+            for(const packet of Packet.all.filter((packet) => packet.name === name)) {
+                packet.callback(data, ServerObject);
             }
         });
 
-        this.InSimHandle.on('connected', () => {
+        InSimHandle.on('connected', () => {
             console.log('[InSim] Connected');
-            Event.fire(EventType.SERVER_CONNECTED, this);
+            Event.fire(EventType.SERVER_CONNECTED, ServerObject);
         });
 
-        this.InSimHandle.on('disconnect', () => {
+        InSimHandle.on('disconnect', () => {
             console.log('[InSim] Disconnected');
-            Event.fire(EventType.SERVER_DISCONNECTED, this);
+            Event.fire(EventType.SERVER_DISCONNECTED, ServerObject);
         });
-    }
 
-    connect(Host: string, Port: number) {
-        if(!(this.InSimHandle instanceof InSim)) return;
+        this.all.push(ServerObject);
 
-        console.log('[InSim] Connecting to ' + Host + ':' + Port);
-        this.InSimHandle.connect({ Host, Port });
+        return ServerObject;
     }
 }
