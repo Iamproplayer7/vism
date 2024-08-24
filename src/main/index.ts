b8a7e379c5bb0d8fc3f44bd8960c07c838ba9312
Iamproplayer7 <iamproplayer7@gmail.com>
2024-08-24 06:40:01 +0300

    export more things

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,3 +1,9 @@
 // classes
 export * from './classes/index.js';
 export { Interval } from './utilities/Interval.js';
+
+export * from 'tsinsim';
+export * from './enums/index.js';
+
+// import plugins
+import '../plugins/core/index.js';
