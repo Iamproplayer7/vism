bd01bf78ebd84599e05863e77557d11e1f8a3f17
Iamproplayer7 <iamproplayer7@gmail.com>
2024-11-02 01:01:29 +0200

    VISM.Vehicle: setMass, setVehicle, setPosition, packet handlers

--- src/main/classes/Vehicle.ts
+++ src/main/classes/Vehicle.ts
@@ -1,10 +1,13 @@
-import { JRRAction, PacketType } from "tsinsim";
-import { IS_JRR, IS_MCI, IS_NPL, IS_PLL, IS_PLP, ObjectInfo } from "tsinsim/packets";
+import { JRRAction, PacketType, PlayerHCapFlags } from "tsinsim";
+import { IS_JRR, IS_MCI, IS_MST, IS_NPL, IS_OBH, IS_PIT, IS_PLH, IS_PLL, IS_PLP, IS_PSF, ObjectInfo, PlayerHCap } from "tsinsim/packets";
 import { Packet } from "./Packet.js";
 import { Event } from "./Event.js";
 import { EventType } from "../enums/event.js";
 import { Player, PlayerGetter } from "./Player.js";
 import { Server } from "./Server.js";
+import Function from "../utilities/Function.js";
+
+const CARS = ['UF1', 'XFG', 'XRG', 'LX4', 'LX6', 'RB4', 'FXO', 'XRT', 'RAC', 'FZ5', 'UFR', 'XFR', 'FXR', 'XRR', 'FZR', 'MRT', 'FBM', 'FOX', 'FO8', 'BF1'];
 
 export const VehicleGetter = {
     all: [] as Vehicle[],
@@ -25,6 +28,8 @@ export const VehicleGetter = {
 
 export interface Vehicle {
     valid: boolean; 
+    isMod: boolean;
+
     getServer(): Server; 
     getPlayer(): Player,
     getPLID(): number; 
@@ -36,13 +41,17 @@ export interface Vehicle {
     getPlate(): string;
     getSpeed(): number;
     getPosition(): { X: number, Y: number, Z: number };
+    setPosition(position: { X: number, Y: number, Z: number }, repair: boolean, heading: number): void;
+    setMass(mass: number): void;
     getHeading(): number;
     getDirection(): number;
+    removeFromTrack(): void;
 }
 
 
 class VehicleInternal implements Vehicle  {
     valid: boolean;
+    isMod: boolean;
 
     private Server: Server;
     private Player: Player;
@@ -60,7 +69,8 @@ class VehicleInternal implements Vehicle  {
 
     constructor(data: IS_NPL, player: Player) {
         this.valid = true;
-        
+        this.isMod = !CARS.includes(data.CName);
+    
         this.Server = player.getServer();
         this.Player = player;
         this.PLID = data.PLID;
@@ -80,8 +90,9 @@ class VehicleInternal implements Vehicle  {
         Packet.on([PacketType.ISP_PLL, PacketType.ISP_PLP], (data: IS_PLL | IS_PLP, server: Server) => {
             if(server !== this.Server || data.PLID !== this.PLID) return;
         
-            Event.fire(EventType.VEHICLE_DESTROYED, this);
+            Event.fire(EventType.VEHICLE_DESTROYED, this, this.Player);
             this.valid = false;
+            this.Player.setVehicle(false);
 
             Packet.offByBind(this);
             VehicleGetter.all = VehicleGetter.all.filter((vehicle) => vehicle !== this);
@@ -92,7 +103,7 @@ class VehicleInternal implements Vehicle  {
             if(server !== this.Server) return;
 
             for(const CompCar of data.Info) {
-                if(CompCar.PLID !== this.PLID) return;
+                if(CompCar.PLID !== this.PLID) continue;
 
                 this.Speed = CompCar.Speed / 32768 * 360;
                 this.Pos = {
@@ -100,11 +111,46 @@ class VehicleInternal implements Vehicle  {
                     Y: CompCar.Y / 65536,
                     Z: CompCar.Z / 65536,
                 };
+
                 this.Heading = CompCar.Heading / 32768 * 180;
                 this.Direction = CompCar.Direction / 182.0444444;
 
                 Event.fire(EventType.VEHICLE_UPDATE, this);
             }
+        }).bind(this);
+
+        // vehicle mass update
+        Packet.on(PacketType.ISP_PLH, (data: IS_PLH, server: Server) => {
+            if(server !== this.Server) return;
+
+            for(const PlayerHCAP of data.HCaps) {
+                if(PlayerHCAP.PLID !== this.PLID) continue;
+                this.HMass = PlayerHCAP.H_Mass;
+            }
+        }).bind(this);
+
+        // vehicle mass update
+        Packet.on(PacketType.ISP_OBH, (data: IS_OBH, server: Server) => {
+            if(server !== this.Server) return;
+            if(data.PLID !== this.PLID) return;
+
+            Event.fire(EventType.VEHICLE_HIT_OBJECT, this, data.Index, data.C.Speed);
+        }).bind(this);
+
+        // vehicle pit stop start
+        Packet.on(PacketType.ISP_PIT, (data: IS_PIT, server: Server) => {
+            if(server !== this.Server) return;
+            if(data.PLID !== this.PLID) return;
+
+            Event.fire(EventType.VEHICLE_PIT_STOP_START, this);
+        }).bind(this);
+
+        // vehicle pit stop end
+        Packet.on(PacketType.ISP_PSF, (data: IS_PSF, server: Server) => {
+            if(server !== this.Server) return;
+            if(data.PLID !== this.PLID) return;
+
+            Event.fire(EventType.VEHICLE_PIT_STOP_END, this);
         }).bind(this);
     }
 
@@ -121,6 +167,31 @@ class VehicleInternal implements Vehicle  {
     getPosition()  { return this.Pos;       }
     getHeading()   { return this.Heading;   };
     getDirection() { return this.Direction; };
+
+    setPosition(position: { X: number, Y: number, Z: number }, repair: boolean = false, heading: number = 0) {
+        // degrees to 256 & reverse
+        heading = heading * 256 / 360;
+        heading = Math.floor(heading > 128 ? heading-128 : heading+128);
+  
+        const packet = new IS_JRR({ PLID: this.getPLID(), JRRAction: repair ? 4 : 5, StartPos: new ObjectInfo({ X: Math.floor(position.X*16), Y: Math.floor(position.Y*16), ZByte: Math.floor(position.Z*4), Heading: heading, Flags: 128 }) })
+        this.getServer().InSimHandle.sendPacket(packet);
+    }
+
+    setMass(mass: number) {
+        this.getServer().InSimHandle.sendPacket(new IS_PLH({
+            HCaps: [ new PlayerHCap( {
+                PLID: this.PLID,
+                Flags: PlayerHCapFlags.MASS,
+                H_Mass: mass
+            }) ]
+        }));
+    }
+
+    removeFromTrack() {
+        this.getServer().InSimHandle.sendPacket(new IS_MST( {
+            Msg: '/spec ' + this.getPlayer().getUsername(),
+        }));
+    }
 }
 
 // creating the vehicle
@@ -133,6 +204,7 @@ Packet.on(PacketType.ISP_NPL, (data: IS_NPL, server: Server) => {
     } 
     
     const vehicle = new VehicleInternal(data, player);
+    player.setVehicle(vehicle);
     VehicleGetter.all.push(vehicle);
     Event.fire(EventType.VEHICLE_CREATED, vehicle, player);
 });
